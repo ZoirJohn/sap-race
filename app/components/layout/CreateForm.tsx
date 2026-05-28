14ae9462e05c55a961b847656c46a6370d4e1204
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-05-28 13:02:14 +0500

    feat: enhance CreateForm to dispatch createCar action; add refs for color and name inputs

--- app/components/layout/CreateForm.tsx
+++ app/components/layout/CreateForm.tsx
@@ -1,21 +1,43 @@
+import { useRef } from "react"
 import { Button, FormControl, InputGroup, Row, Stack } from "react-bootstrap"
+import { useDispatch } from "react-redux"
 import { Form } from "react-router"
+import { createCar } from "~/api/client"
+import type { RacersStoreDispatch } from "~/type"
 
 export default function CreateForm() {
+    const dispatch = useDispatch<RacersStoreDispatch>()
+    const colorField = useRef<HTMLInputElement>(null)
+    const nameField = useRef<HTMLInputElement>(null)
+    function generateCar() {
+        if (!!nameField.current?.value) {
+            dispatch(
+                createCar(
+                    nameField.current?.value!,
+                    colorField.current?.value!,
+                ),
+            )
+        }
+    }
     return (
         <Form>
             <Stack direction="horizontal" gap={1}>
                 <InputGroup className="max-w-20">
-                    <FormControl type="color" className="grow-0"></FormControl>
+                    <FormControl
+                        type="color"
+                        className="grow-0"
+                        ref={colorField}
+                    ></FormControl>
                 </InputGroup>
                 <InputGroup className="max-w-80">
                     <FormControl
                         type="text"
                         className="grow-0"
                         placeholder="Name"
+                        ref={nameField}
                     ></FormControl>
                 </InputGroup>
-                <Button>CREATE</Button>
+                <Button onClick={generateCar}>CREATE</Button>
             </Stack>
         </Form>
     )
